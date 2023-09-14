daa621ee5d441f5b8f376a028ce8ed8803d78100
Andrew Bungcayao <thedevandrew@gmail.com>
2023-09-14 01:24:30 -0400

    Create initial passport demo using express, mongodb, ejs, and passport npm package. Integrate mongodb database with user sign-up and login credentials via ejs. Temporarily store session data in memory.

---
+++ app.js
@@ -0,0 +1,144 @@
+require("dotenv").config();
+const bcrypt = require("bcryptjs");
+const express = require("express");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const passport = require("passport");
+const path = require("path");
+const Schema = mongoose.Schema;
+const session = require("express-session");
+
+const mongoDB = process.env.MONGO_URI;
+mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+const User = mongoose.model(
+  "User",
+  new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+  })
+);
+
+const app = express();
+
+app.set("views", __dirname);
+app.set("view engine", "ejs");
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "Incorrect password" });
+        }
+      });
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+const authMiddleware = (req, res, next) => {
+  if (!req.user) {
+    if (!req.session.messages) {
+      req.session.messages = [];
+    }
+    req.session.messages.push("You can't access that page before logon.");
+    res.redirect("/");
+  } else {
+    next();
+  }
+};
+
+app.get("/restricted", authMiddleware, (req, res) => {
+  if (!req.session.pageCount) {
+    req.session.pageCount = 1;
+  } else {
+    req.session.pageCount++;
+  }
+  res.render("restricted", { pageCount: req.session.pageCount });
+});
+
+app.get("/", (req, res) => {
+  console.log("res.locals =", res.locals);
+  let messages = [];
+  if (req.session.messages) {
+    messages = req.session.messages;
+    req.session.messages = [];
+  }
+  res.render("index", { messages });
+});
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
+
+app.get("/sign-up", (req, res) => {
+  res.render("sign-up-form");
+});
+
+app.post("/sign-up", async (req, res, next) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = await User.create({
+      username: req.body.username,
+      password: hashedPassword,
+    });
+    console.log(hashedPassword);
+    console.log("user =", user);
+    res.redirect("/");
+  } catch (err) {
+    return next(err);
+  }
+});
+
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+    failureMessage: true,
+  })
+);
+
+app.get("/log-out", (req, res, next) => {
+  console.log("req.logout =", req.logout);
+  req.session.destroy(function (err) {
+    res.redirect("/");
+  });
+});
